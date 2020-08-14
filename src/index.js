b35c80e2fb8cf6324a2ca281eb1e8395a7ec0224
= <nassiromid@gmail.com>
2020-08-14 11:46:17 -0700

    Replaced ActivePlayer component with App component in index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import ActivePlayer from './components/ActivePlayer';
-
-ReactDOM.render(<ActivePlayer />, document.getElementById('root')
+ReactDOM.render(<App />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
